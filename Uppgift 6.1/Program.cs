835eda92d24bab227dff89c889704712d77c580f
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-14 10:45:48 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addera(5, 7, 9));
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int sum = tal1 + tal2 + tal3;
+            return sum;
+        }
+    }
+}
